59aaf66cc8a22173a6296389bb1e18899fdedad8
Roushan Raj <68936132+Roushan-official@users.noreply.github.com>
2026-08-20 11:42:15 +0530

    Update API URL to use ngrok for tunneling

--- drx.py
+++ drx.py
@@ -18,7 +18,7 @@ else:
 
 bot = telebot.TeleBot(config['token'])
 # Agar API isi VPS par hai toh '127.0.0.1' use karein, varna VPS ka IP dalein
-API_URL = "http://34.126.208.96:8080/hit" 
+API_URL = "https://d9d1-2401-4900-1caa-449c-7c60-e86a-8561-c4ab.ngrok-free.app:8080/hit" 
 AUTH_TOKEN = "Hazard_DDOS"
 
 # Database files
